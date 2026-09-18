e17e4368cea59d29c39a9e0fa6f7e66c680e090b
Aarón Martíns <aaronmavi07@gmail.com>
2026-09-18 14:42:38 +0200

    Ahora cuando el perosnaje toca los pinchos se pone en rojo y no te deja moverte

--- prueba-aaron/Escenas/Personaje/personaje.gd
+++ prueba-aaron/Escenas/Personaje/personaje.gd
@@ -1,22 +1,27 @@
 extends CharacterBody2D
-
+@export var material_personaje_rojo: ShaderMaterial
 @export var animacion: AnimatedSprite2D
 @export var area_2d : Area2D
+
 var jumpCont: int = 3
 var _velocidad: float = 100.0
 var _velocidad_salto: float = -300.0
 var gravedad: bool = true
+var _muerto: bool
 
 
 func _ready() -> void:
 	area_2d.body_entered.connect(_on_area_2d_body_entered)
 
+
 func _physics_process(delta: float) -> void:
-	
-	#gravedad
+	if _muerto:
+		return
+	#Toggle gravedad 0
 	if Input.is_action_just_pressed("E"):
 		gravedad = not gravedad
-		
+	
+	#Cálculos de gravedad
 	if not is_on_floor():
 		if gravedad: #Si la gravedad cero no esta activada (true)
 			velocity = velocity + get_gravity() * delta
@@ -25,13 +30,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		jumpCont = 3
 		
-	#salto
+	#Salto
 	if Input.is_action_just_pressed("ui_accept") and jumpCont != 0:
 		velocity.y = _velocidad_salto
 		jumpCont = jumpCont - 1
 	
 		
-	#movimiento lateral
+	#Movimiento lateral
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -_velocidad
 		animacion.flip_h = false
@@ -44,8 +49,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 	move_and_slide()
 	
-	#animacion
-	
+	#Animaciónes
 	if !is_on_floor():
 		animacion.play("saltar")
 	elif velocity.x != 0:
@@ -55,4 +59,5 @@ func _physics_process(delta: float) -> void:
 		
 		
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("muerto")
+	animacion.material = material_personaje_rojo
+	_muerto = true
